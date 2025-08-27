6cd7c9287ab5b5719aba796a5419425c15c41500
MRidwansah <muhamadridwang6@gmail.com>
2025-08-27 08:06:51 +0700

    Add files via upload

---
+++ Wan_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wan_project1;
+
+/**
+ *
+ * @author acer
+ */
+public class Wan_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Muhamad Ridwansah");
+        System.out.println("NPM     : 2440506079");
+        System.out.println("Alamat  : Tidar Sari Kota Magelang");
+        System.out.println("No_HP   : 081216479749");
+    }
+}
